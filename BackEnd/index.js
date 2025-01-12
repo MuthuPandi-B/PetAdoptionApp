fd59e8750bcc39ba2d7cd22f6f3de72b28e4d97a
MuthuPandi-B <muthup61@gmail.com>
2025-01-12 20:18:28 +0530

    cors error corrected

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -15,29 +15,31 @@ import fosterPetRoutes from "./Routers/fosterPetRoutes.js";
 dotenv.config();
 const app = express();
 
-app.use(cors({
-     origin: 'https://adopt-a-pets.netlify.app',
-     origin: 'http://localhost:5173',
-     credentials: true,
-}));
+app.use(
+  cors({
+    origin: "https://adopt-a-pets.netlify.app",
+
+    credentials: true,
+  })
+);
 app.use(express.json());
 connectDB();
 
 app.get("/", (req, res) => {
-    res.send("Welcome to pet adoption backend");
+  res.send("Welcome to pet adoption backend");
 });
-app.use ("/api/auth", authRoutes);
+app.use("/api/auth", authRoutes);
 app.use("/api/applications", applicationRoutes);
 app.use("/api/pets", petRoutes);
-app.use('/api/reviews', reviewRoutes);
-app.use('/api/messages', messageRoutes);
-app.use('/api/appointments', appointmentRoutes);
-app.use("/api",userRoutes);
-app.use('/api/favorites', favoriteRoutes);
 app.use("/api/reviews", reviewRoutes);
-app.use('/api/fosterpets', fosterPetRoutes);
+app.use("/api/messages", messageRoutes);
+app.use("/api/appointments", appointmentRoutes);
+app.use("/api", userRoutes);
+app.use("/api/favorites", favoriteRoutes);
+app.use("/api/reviews", reviewRoutes);
+app.use("/api/fosterpets", fosterPetRoutes);
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
