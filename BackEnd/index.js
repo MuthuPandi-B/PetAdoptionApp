70b4c4ea6860ccf4d0f4c8bb2c196fa5b2014054
MuthuPandi-B <muthup61@gmail.com>
2025-01-12 17:50:45 +0530

    localhost added

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -17,6 +17,7 @@ const app = express();
 
 app.use(cors({
      origin: 'https://adopt-a-pets.netlify.app',
+     origin: 'http://localhost:5173',
      credentials: true,
 }));
 app.use(express.json());
